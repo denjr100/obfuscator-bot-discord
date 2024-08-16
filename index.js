49d559c4661447e36338c587314e20b551ed1c5e
Abukhalid <110600419+denjr100@users.noreply.github.com>
2024-08-17 01:27:36 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,97 @@
+// This bot is free made by :k.xj (Abukhalid)
+// discord : https://discord.gg/2N2WqpUtEy
+//__    __     _____         ______  ________   ______   _______   ________ 
+//|  \  |  \   |     \       /      \|        \ /      \ |       \ |        \
+//| $$\ | $$    \$$$$$      |  $$$$$$\\$$$$$$$$|  $$$$$$\| $$$$$$$\| $$$$$$$$
+//| $$$\| $$      | $$      | $$___\$$  | $$   | $$  | $$| $$__| $$| $$__    
+//| $$$$\ $$ __   | $$       \$$    \   | $$   | $$  | $$| $$    $$| $$  \   
+//| $$\$$ $$|  \  | $$       _\$$$$$$\  | $$   | $$  | $$| $$$$$$$\| $$$$$   
+//| $$ \$$$$| $$__| $$      |  \__| $$  | $$   | $$__/ $$| $$  | $$| $$_____ 
+//| $$  \$$$ \$$    $$       \$$    $$  | $$    \$$    $$| $$  | $$| $$     \
+ //\$$   \$$  \$$$$$$         \$$$$$$    \$$     \$$$$$$  \$$   \$$ \$$$$$$$$
+                                                                           
+                                                                           
+                                                                           
+
+
+const { Client, GatewayIntentBits, ButtonBuilder, ActionRowBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle, Events, AttachmentBuilder } = require('discord.js');
+const JavaScriptObfuscator = require('javascript-obfuscator');
+const fs = require('fs');
+const path = require('path');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+                                                                                                                                                function _0x1bde(){var _0x137dc7=['log','discord.gg/2N2WqpUtEy','3743127CYgmkl','1;93','ready','\x1b[0m','516830HfRUxo','1;91','لﺎﻐﺷ\x20ﻦﻳﻻ\x20نﻭﺍ\x20-\x20ﻲﻔﻄﻳﺎﻣ\x20نﺎﺸﻋ\x20ءﻲﺷ\x20لﺪﻌﺗ\x20وﺍ\x20ﻢﺳﺍ\x20ﺮﻴﻐﺗﻻ','3459OaTxem','455BokiEY','6hDvZsj','392OdEcUj','1021339CdKQaV','24904ZuBSNz','1;34','40ZRLLpR','Code\x20by\x20NJ\x20-\x20ﺪﻟﺎﺧ\x20ﻮﺑﺍ','1;35','tag','2203945ZyjQbh','1359316fqUgqg'];_0x1bde=function(){return _0x137dc7;};return _0x1bde();}function _0x1e0a(_0x40a88c,_0x290c66){var _0x1bde37=_0x1bde();return _0x1e0a=function(_0x1e0a2b,_0x5599d3){_0x1e0a2b=_0x1e0a2b-0x18d;var _0x57413b=_0x1bde37[_0x1e0a2b];return _0x57413b;},_0x1e0a(_0x40a88c,_0x290c66);}var _0x39f10e=_0x1e0a;(function(_0x236bff,_0x59d3d9){var _0x53e56=_0x1e0a,_0x4102be=_0x236bff();while(!![]){try{var _0x3721f7=parseInt(_0x53e56(0x195))/0x1+parseInt(_0x53e56(0x19b))/0x2*(parseInt(_0x53e56(0x198))/0x3)+-parseInt(_0x53e56(0x18e))/0x4+-parseInt(_0x53e56(0x18d))/0x5*(-parseInt(_0x53e56(0x19a))/0x6)+parseInt(_0x53e56(0x199))/0x7*(parseInt(_0x53e56(0x19d))/0x8)+-parseInt(_0x53e56(0x191))/0x9+parseInt(_0x53e56(0x19f))/0xa*(-parseInt(_0x53e56(0x19c))/0xb);if(_0x3721f7===_0x59d3d9)break;else _0x4102be['push'](_0x4102be['shift']());}catch(_0x449381){_0x4102be['push'](_0x4102be['shift']());}}}(_0x1bde,0x3f308));function coloredLog(_0x281a46,_0x35b1b5){var _0x4b0550=_0x1e0a;console[_0x4b0550(0x18f)]('\x1b['+_0x35b1b5+'m'+_0x281a46+_0x4b0550(0x194));}client['on'](_0x39f10e(0x193),()=>{var _0x49a543=_0x39f10e;coloredLog('ﻢﺳﺄﺑ\x20ﻞﻴﺠﺴﺗ\x20ﻢﺗ\x20:\x20'+client['user'][_0x49a543(0x1a2)],_0x49a543(0x192)),coloredLog(_0x49a543(0x197),_0x49a543(0x196)),coloredLog(_0x49a543(0x190),_0x49a543(0x196)),coloredLog(_0x49a543(0x1a0),_0x49a543(0x19e)),coloredLog('\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20_\x20_\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20_\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20__\x20\x20\x20\x20\x20\x20_\x20\x20_\x20\x0a\x20\x20\x20\x20\x20\x20__|\x20(_)___\x20\x20___\x20___\x20\x20_\x20__\x20__|\x20|\x20\x20__\x20_\x20\x20__\x20_\x20\x20\x20/\x20/\x20__\x20\x20(_)(_)\x0a\x20\x20\x20\x20\x20/\x20_`\x20|\x20/\x20__|/\x20__/\x20_\x20\x5c|\x20\x27__/\x20_`\x20|\x20/\x20_`\x20|/\x20_`\x20|\x20/\x20/\x20\x27_\x20\x5c\x20|\x20||\x20|\x0a\x20\x20\x20\x20|\x20(_|\x20|\x20\x5c__\x20\x5c\x20(_|\x20(_)\x20|\x20|\x20|\x20(_|\x20||\x20(_|\x20|\x20(_|\x20|/\x20/|\x20|\x20|\x20|\x20||\x20||\x20|\x0a\x20\x20\x20\x20\x20\x5c__,_|_|___/\x5c___\x5c___/|_|\x20\x20\x5c__,_(_)__,\x20|\x5c__,\x20/_/\x20|_|\x20|_|/\x20|/\x20|\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20|___/\x20|___/\x20\x20\x20\x20\x20\x20\x20\x20\x20|__/__/\x20\x0a\x20\x20\x20\x20',_0x49a543(0x1a1));});
+// discord : https://discord.gg/2N2WqpUtEy   
+client.on('messageCreate', async (message) => {
+    if (message.content === 'nj') { // الكلمة الي بيرد عليها البوت
+        const button = new ButtonBuilder()
+            .setCustomId('getModlCode') // لاتعدل شيء
+            .setLabel('اضغط على زر و شفر كودك') // اسم زر
+            .setStyle(ButtonStyle.Danger) // لون الزر
+            .setEmoji("<a:12459227013992981413:1246287523366567977> "); // ايمويجي الزر
+
+        const row = new ActionRowBuilder()
+            .addComponents(button);
+
+var _0x4e8b65=_0x35c1;function _0x35c1(_0x4d1371,_0xf3d4d2){var _0x1f3f3a=_0x1f3f();return _0x35c1=function(_0x35c105,_0x28c77b){_0x35c105=_0x35c105-0x1ac;var _0x144b94=_0x1f3f3a[_0x35c105];return _0x144b94;},_0x35c1(_0x4d1371,_0xf3d4d2);}function _0x1f3f(){var _0x475d1e=['4597480goJdgF','3482460bVtosP','2367172zkKhMt','2057874NbZjHq','3243555CKmPqW','1987968kYpQlM','2UGLDLv','**https://discord.gg/2N2WqpUtEy\x20||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||||||||||@here**','channel','5PswlKa','send','90061rEPfnB'];_0x1f3f=function(){return _0x475d1e;};return _0x1f3f();}(function(_0x4b55de,_0x38baaf){var _0x5bd94d=_0x35c1,_0x3cf318=_0x4b55de();while(!![]){try{var _0x1598ca=-parseInt(_0x5bd94d(0x1b3))/0x1*(-parseInt(_0x5bd94d(0x1ae))/0x2)+parseInt(_0x5bd94d(0x1ad))/0x3+-parseInt(_0x5bd94d(0x1b6))/0x4*(-parseInt(_0x5bd94d(0x1b1))/0x5)+-parseInt(_0x5bd94d(0x1b7))/0x6+-parseInt(_0x5bd94d(0x1ac))/0x7+-parseInt(_0x5bd94d(0x1b4))/0x8+parseInt(_0x5bd94d(0x1b5))/0x9;if(_0x1598ca===_0x38baaf)break;else _0x3cf318['push'](_0x3cf318['shift']());}catch(_0x479347){_0x3cf318['push'](_0x3cf318['shift']());}}}(_0x1f3f,0x558d5),await message[_0x4e8b65(0x1b0)][_0x4e8b65(0x1b2)]({'content':_0x4e8b65(0x1af),'components':[row]}));    }
+});
+// discord : https://discord.gg/2N2WqpUtEy   
+client.on(Events.InteractionCreate, async interaction => {
+    if (interaction.isButton()) {
+        if (interaction.customId === 'getModlCode') { // لاتعدل شيء
+            const modal = new ModalBuilder()
+                .setCustomId('codeModal') // لاتعدل شيء
+                .setTitle('اكتب كودك'); // العنوان
+
+            const codeInput = new TextInputBuilder()
+                .setCustomId('codeInput') // لاتعدل شيء
+                .setLabel('الكود') // لاتعدل شيء
+                .setStyle(TextInputStyle.Paragraph)
+                .setPlaceholder('اكتب كودك هنا') // لاتعدل شيء
+                .setRequired(true);
+
+            const actionRow = new ActionRowBuilder().addComponents(codeInput);
+            modal.addComponents(actionRow);
+
+            await interaction.showModal(modal);
+        }
+    } else if (interaction.isModalSubmit()) {
+        if (interaction.customId === 'codeModal') {
+            const code = interaction.fields.getTextInputValue('codeInput');
+
+            const obfuscatedCode = JavaScriptObfuscator.obfuscate(code, {
+                compact: true,
+                controlFlowFlattening: true,
+            }).getObfuscatedCode();
+
+            const filePath = path.join(__dirname, 'obfuscatedCode.js');
+            fs.writeFileSync(filePath, obfuscatedCode);
+
+            const attachment = new AttachmentBuilder(filePath);
+// اذا تبغى الرد مو مخفي اكتب false
+            await interaction.reply({ content: 'هذا كودك المشفر', ephemeral: true, files: [attachment] }); // الرد المخفي
+
+            fs.unlinkSync(filePath);
+        }
+    }
+});
+
+// توكن بوتك هنا 
+
+client.login('');
+
+// This bot is free made by :k.xj (Abukhalid)
+// discord : https://discord.gg/2N2WqpUtEy
+//__    __     _____         ______  ________   ______   _______   ________ 
+//|  \  |  \   |     \       /      \|        \ /      \ |       \ |        \
+//| $$\ | $$    \$$$$$      |  $$$$$$\\$$$$$$$$|  $$$$$$\| $$$$$$$\| $$$$$$$$
+//| $$$\| $$      | $$      | $$___\$$  | $$   | $$  | $$| $$__| $$| $$__    
+//| $$$$\ $$ __   | $$       \$$    \   | $$   | $$  | $$| $$    $$| $$  \   
+//| $$\$$ $$|  \  | $$       _\$$$$$$\  | $$   | $$  | $$| $$$$$$$\| $$$$$   
+//| $$ \$$$$| $$__| $$      |  \__| $$  | $$   | $$__/ $$| $$  | $$| $$_____ 
+//| $$  \$$$ \$$    $$       \$$    $$  | $$    \$$    $$| $$  | $$| $$     \
+ //\$$   \$$  \$$$$$$         \$$$$$$    \$$     \$$$$$$  \$$   \$$ \$$$$$$$$
+                                                                           
+                                                                           
+                                                                           
